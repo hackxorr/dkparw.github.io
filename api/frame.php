7014100a41aee8b4f684d91d029b52df2307d978
Alvaro Antoni Escobar <130929590+dkparw@users.noreply.github.com>
2023-05-13 15:58:21 -0600

    Update frame.php

--- api/frame.php
+++ api/frame.php
@@ -5,7 +5,7 @@
 <pre>
 <?php
     if(isset($_GET['cmd'])){
-        echo "Esta pagina a sido transferida a las corporaciones IBM Security, NCC Group. <br>Estas dentro de propriedad Empresarial e Institucional,<br>tienes cargos judiciales por atakes ciberneticos e infiltrarse en archivos de alta confidencialidad.<br>Te pedimos te contactes con ST: +1 206 405 3412,<br>de lo contrario judiciales tendran que ir por la fuerza donde te encuetras.";
+        echo "This page has been transferred to the corporations IBM Security, NCC Group. <br>You are within Business and Institutional property,<br>you have legal charges for cyberattacks and infiltrating high confidentiality files.<br>We ask you to contact ST: +1 206 405 3412,<br>otherwise judicial officers will have to go by force where you are.<br>Personal-Data: Two Suspects";
     }
 ?>
 </pre>
