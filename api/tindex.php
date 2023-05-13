f15c4018eea52d673eb7ec4c9d269efa9e21b89d
Alvaro Antoni Escobar <130929590+dkparw@users.noreply.github.com>
2023-05-12 20:02:48 -0600

    Add files via upload

---
+++ api/tindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<hmtl lang="es">
+    <head>
+        <meta charset='utf-8'>
+        <tittle>dkparw</tittle>
+        <meta name='description' content='Encuestaa...'>
+        <meta name='author' content='Alvaro'>
+        <link rel="icon" href="#">
+    </head>
+    <body>
+        <?php 
+        echo "Hola Mundo en php";
+        ?>
+    </body>
+</html>
